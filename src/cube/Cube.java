dc4040f6d490a1c1a6729b4d807bdad07f616c59
nsaspook <nsaspook@nsaspook.com>
2022-12-02 17:48:05 -0800

    up

--- src/cube/Cube.java
+++ src/cube/Cube.java
@@ -5,10 +5,8 @@
  */
 package cube;
 
-import java.util.Scanner;
 import java.sql.*;
 import java.util.Properties;
-import java.net.Socket;
 
 /**
  *
@@ -16,6 +14,7 @@ import java.net.Socket;
  */
 public class Cube {
 
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
 
         // source and dest IP configurations
@@ -24,99 +23,46 @@ public class Cube {
 
 //        String ttl_eth_host = "192.168.0.7";
 //        String mysql_host = "jdbc:mariadb://10.5.2.94/";
+        Properties connConfig = new Properties();
+        connConfig.setProperty("user", "minty");
+        connConfig.setProperty("password", "");
+
+        Statement stmt = null;
+        ResultSet rs = null;
 
         try {
-            // Connect to the TTL to ETH server
-            Socket socket = new Socket(ttl_eth_host, 20108);
+            Connection conn = DriverManager.getConnection(mysql_host, connConfig);
 
-            Properties connConfig = new Properties();
-            connConfig.setProperty("user", "minty");
-            connConfig.setProperty("password", "");
+            stmt = conn.createStatement();
 
-            Scanner s = new Scanner(socket.getInputStream());
-            System.err.println("Scanner running.");
-            while (s.hasNextLine()) {
-                String fftd = "";
-                String tstamp = "";
-                String hosts = "";
-                String cmark = "";
-                try {
-                    String line = s.nextLine();
-                    String[] token = line.split(",");
+            stmt.executeUpdate("USE hits");
 
-                    if (token[0].equals("  1")) {
-                        tstamp = token[11];
-                        cmark = token[12];
-                        System.out.println(String.format("dtype = %3s  device = %s : %s", token[0], token[1], line, tstamp));
-                        token[2] = "";
-                    }
-                    if (token[0].equals("  2")) {
-                        System.out.println(String.format("dtype = %3s  device = %s cpu = %s host = %s : %s", token[0], token[1], token[2], token[6], line));
-                        hosts = token[6];
-                        cmark = token[7];
-                        token[6] = "";
-                        token[7] = "";
-                    }
-                    if (token[0].equals("  8")) {
-                        System.out.println(String.format("dtype = %3s  device = %s hit  low = %s : %s", token[0], token[1], token[2], line));
-                        fftd = token[2];
-                        cmark = token[3];
-                        token[2] = "";
-                        token[3] = "";
-                    }
-                    if (token[0].equals("  9")) {
-                        System.out.println(String.format("dtype = %3s  device = %s hit high = %s : %s", token[0], token[1], token[2], line));
-                        fftd = token[2];
-                        cmark = token[3];
-                        token[2] = "";
-                        token[3] = "";
-                    }
+            rs = stmt.executeQuery(
+                    "SELECT timestamp "
+                    + "FROM imu "
+                    + "order by timestamp ");
 
-                    try {
-                        Connection conn = DriverManager.getConnection(mysql_host, connConfig);
+            stmt.executeUpdate("DROP TABLE IF EXISTS vevent");
+            stmt.executeUpdate(
+                    "CREATE TABLE vevent ("
+                    + "priKey INT NOT NULL AUTO_INCREMENT, "
+                    + "dataField VARCHAR(255), PRIMARY KEY (priKey))");
 
-                        if (cmark.equals("IMU")) {
-                            // Prepare INSERT Statement to Add IMU data
-                            try ( PreparedStatement prep = conn.prepareStatement(
-                                    "INSERT INTO hits.imu (dtype, device, cpu, str, fft, tstamp, host) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                                    Statement.RETURN_GENERATED_KEYS)) {
-                                // Add IMU packet data
-                                prep.setString(1, token[0]);
-                                prep.setString(2, token[1]);
-                                prep.setString(3, token[2]);
-                                prep.setString(4, line);
-                                prep.setString(5, fftd);
-                                prep.setString(6, tstamp);
-                                prep.setString(7, hosts);
-                                prep.addBatch();
+            stmt.executeUpdate(
+                    "INSERT INTO vevent (dataField) "
+                    + "select (str) "
+                    + "from imu "
+                    + "where dtype like '  1' AND fft > '  9' LIMIT 20000", 
+                    Statement.RETURN_GENERATED_KEYS);
 
-                                int[] updateCounts = prep.executeBatch();
-                                for (int count : updateCounts) {
-                                    // Print Counts
-//                            System.out.println(count);
-                                }
-                            }
-                        }
-                        conn.close();
-                    } catch (SQLException ex) {
-                        // handle any errors
-                        System.out.println("SQLException: " + ex.getMessage());
-                        System.out.println("SQLState: " + ex.getSQLState());
-                        System.out.println("VendorError: " + ex.getErrorCode());
-                    }
-                } catch (Exception e) {
-                }
-            }
-            // Close our streams
-            socket.close();
-            s.close();
-            System.err.println("Lost communication with the IMU socket. Exiting.");
-            System.exit(1);
-
-        } catch (Exception e) {
-            System.err.println("Unable to open the TCP socket. Exiting.");
-            System.exit(1);
+            conn.close();
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
         }
+
     }
 
 }
